fe2730de6880a5038ecfab6cb7ba365000cb607e
tmerchan <tamara.merchan.tm@gmail.com>
2025-10-01 20:31:45 -0300

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Tabs } from 'expo-router';
-import { Home, Users, CircleUser, Bell, Clock } from 'lucide-react-native';
+import { Home, Users, CircleUser } from 'lucide-react-native';
 
 const BRAND = '#6B8E23';
 
